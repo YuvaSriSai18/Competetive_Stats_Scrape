bccfdcc130441ec6331909720616114bd0f0df62
mohan-1698 <mohanvenky201@gmail.com>
2026-02-11 15:28:56 +0530

    Made init.py and changed the .sh and .yaml files

--- build.sh
+++ build.sh
@@ -1,10 +1,23 @@
-#!/bin/bash
+# #!/bin/bash
+# set -e
+
+# echo "Installing Python dependencies..."
+# pip install -r requirements.txt
+
+# echo "Installing Playwright browsers..."
+# playwright install chromium
+
+# echo "Build complete!"
+
+
+#!/usr/bin/env bash
 set -e
 
 echo "Installing Python dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Installing Playwright browsers..."
-playwright install chromium
+playwright install chromium --with-deps
 
 echo "Build complete!"
